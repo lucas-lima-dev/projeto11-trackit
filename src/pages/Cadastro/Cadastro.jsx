c739465f4c0a5246678d3b1aa17654d0dec16e41
lucas-lima-dev <lucas.sdslima@gmail.com>
2022-12-16 20:09:12 -0300

    Implementando cadastro

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,52 +1,84 @@
 import { PageContainer, Form } from "./styled";
 import logo from "../../assets/LogoCadastro.png";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../constants/urls";
+import axios from "axios";
 
 export default function Cadastro() {
-    const [email, setEmail] = useState("");
-    const [senha, setSenha] = useState("");
-    const [nome, setNome] = useState("");
-    const [foto, setFoto] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+  const navigate = useNavigate();
 
-    function cadastro() {}
-  
-    return (
-      <PageContainer>
+  function createAccount(event) {
+    event.preventDefault();
+
+    const URL = `${BASE_URL}/auth/sign-up`;
+
+    const body = {
+      email,
+      name,
+      image,
+      password,
+    };
+
+    console.log(body)
+
+    axios
+      .post(URL, body)
+      .then((res) => {
+        alert("Cadastro realizado com sucesso");
+        navigate("/");
+        setEmail("");
+        setPassword("");
+        setName("");
+        setImage("");
         
-        <img src={logo} atl="" />
-        
-        <Form onSubmit={cadastro}>
-          <input
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            placeholder="senha"
-            value={senha}
-            onChange={(e) => setSenha(e.target.value)}
-            required
-          />
-           <input
-            placeholder="nome"
-            value={nome}
-            onChange={(e) => setNome(e.target.value)}
-            required
-          />
-           <input
-            placeholder="foto"
-            value={foto}
-            onChange={(e) => setFoto(e.target.value)}
-            required
-          />
-          <button type="submit">Entrar</button>
-        </Form>
-        <Link to={"/"}>
-          <p>Já tem uma conta? Faça login!</p>
-        </Link>
-        
-      </PageContainer>
-    );
+      })
+
+      .catch((err) => alert(err.response.data));
   }
+
+  return (
+    <PageContainer>
+      <img src={logo} atl="" />
+
+      <Form onSubmit={createAccount}>
+        <input
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          dataIdentifier="input-email"
+        />
+        <input
+          placeholder="senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          dataIdentifier="input-password"
+        />
+        <input
+          placeholder="nome"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          dataIdentifier="input-name"
+        />
+        <input
+          placeholder="foto"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+          required
+          dataIdentifier="input-photo"
+        />
+        <button type="submit">Entrar</button>
+      </Form>
+      <Link to={"/"}>
+        <p>Já tem uma conta? Faça login!</p>
+      </Link>
+    </PageContainer>
+  );
+}
